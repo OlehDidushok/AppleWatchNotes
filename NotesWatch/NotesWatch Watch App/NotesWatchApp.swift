42fe5a0fe255a323884c0706fa63e10bb4b3ecaa
Oleh Didushok <didushokov@gmail.com>
2024-11-08 12:59:15 +0200

    Initial coomit, added resources

---
+++ NotesWatch/NotesWatch Watch App/NotesWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesWatchApp.swift
+//  NotesWatch Watch App
+//
+//  Created by Oleh on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
